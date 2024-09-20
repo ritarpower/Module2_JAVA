bc12265ddd479717bdb6825a9021489c242514b2
Minh Phuc <caoquangminhphuc@gmail.com>
2024-09-20 13:04:22 +0700

    add file

--- ExerciseModule2JAVA/src/MoHinhMVCTest/Controller/StudentController.java
+++ ExerciseModule2JAVA/src/MoHinhMVCTest/Controller/StudentController.java
@@ -7,6 +7,8 @@ import MoHinhMVCTest.Service.Student.StudentService;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StudentController {
     private static final Scanner sc = new Scanner(System.in);
@@ -26,14 +28,26 @@ public class StudentController {
     public void addStudent() {
         System.out.println("Nhập mã học viên:");
         String code = sc.nextLine();
-        System.out.println("Nhập tên học viên:");
-        String name = sc.nextLine();
-        System.out.println("Nhập ngày sinh học viên:");
-        LocalDate birthDate = LocalDate.parse(sc.nextLine());
-        System.out.println("Nhập email học viên:");
-        String email = sc.nextLine();
-        System.out.println("Nhập lớp học viên:");
-        String classCode = sc.nextLine();
+        String name;
+        do {
+            System.out.println("Nhập tên học viên:");
+            name = sc.nextLine();
+        } while (!isValidName(name));
+        LocalDate birthDate;
+        do {
+            System.out.println("Nhập ngày sinh học viên theo yyyy-mm-dd :");
+            birthDate = LocalDate.parse(sc.nextLine());
+        } while (!isValidBirthDate(birthDate));
+        String email;
+        do {
+            System.out.println("Nhập email học viên:");
+            email = sc.nextLine();
+        } while (!isValidEmail(email));
+        String classCode;
+        do {
+            System.out.println("Nhập lớp học viên theo Cxx24Mx :");
+            classCode = sc.nextLine();
+        } while (!isValidClassCode(classCode));
         Student student = new Student(code, name, birthDate, email, classCode);
         studentService.addStudent(student);
         System.out.println("Thêm mới thành công!");
@@ -44,17 +58,29 @@ public class StudentController {
         String code = sc.nextLine();
         Student student = studentService.findStudentByCode(code);
         if (student != null) {
-            System.out.println("Nhập tên học viên:");
-            String name = sc.nextLine();
+            String name;
+            do {
+                System.out.println("Nhập tên học viên:");
+                name = sc.nextLine();
+            } while (!isValidName(name));
             student.setName(name);
-            System.out.println("Nhập ngày sinh học viên:");
-            LocalDate birthDate = LocalDate.parse(sc.nextLine());
+            LocalDate birthDate;
+            do {
+                System.out.println("Nhập ngày sinh học viên theo yyyy-mm-dd :");
+                birthDate = LocalDate.parse(sc.nextLine());
+            } while (!isValidBirthDate(birthDate));
             student.setBirthDate(birthDate);
-            System.out.println("Nhập email học viên:");
-            String email = sc.nextLine();
+            String email;
+            do {
+                System.out.println("Nhập email học viên:");
+                email = sc.nextLine();
+            } while (!isValidEmail(email));
             student.setEmail(email);
-            System.out.println("Nhập lớp học viên:");
-            String classCode = sc.nextLine();
+            String classCode;
+            do {
+                System.out.println("Nhập lớp học viên theo Cxx24Mx :");
+                classCode = sc.nextLine();
+            } while (!isValidClassCode(classCode));
             student.setClassCode(classCode);
             studentService.updateStudent(student);
             System.out.println("Đã chỉnh sửa thành công!");
@@ -69,5 +95,29 @@ public class StudentController {
 
     public void deleteStudent(Student student) {
         studentService.deleteStudent(student);
+    }
+
+    public boolean isValidName(String name) {
+        Pattern pattern = Pattern.compile("^[\\p{L} .'-]+$");
+        Matcher matcher = pattern.matcher(name);
+        return matcher.matches();
+    }
+
+    public boolean isValidBirthDate(LocalDate birthDate) {
+        Pattern pattern = Pattern.compile("^\\d{4}[-|/]\\d{2}[-|/]\\d{2}$");
+        Matcher matcher = pattern.matcher(birthDate.toString());
+        return matcher.matches();
+    }
+
+    public boolean isValidEmail(String email) {
+        Pattern pattern = Pattern.compile("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$");
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    public boolean isValidClassCode(String classCode) {
+        Pattern pattern = Pattern.compile("^C\\d{2}24M\\d$");
+        Matcher matcher = pattern.matcher(classCode);
+        return matcher.matches();
     }
 }
--- ExerciseModule2JAVA/src/MoHinhMVCTest/Controller/TeacherController.java
+++ ExerciseModule2JAVA/src/MoHinhMVCTest/Controller/TeacherController.java
@@ -7,6 +7,8 @@ import MoHinhMVCTest.Service.Teacher.TeacherService;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TeacherController {
     private static final Scanner sc = new Scanner(System.in);
@@ -24,14 +26,26 @@ public class TeacherController {
     public void addTeacher() {
         System.out.println("Nhập mã giảng viên: ");
         String code = sc.nextLine();
-        System.out.println("Nhập tên giảng viên: ");
-        String name = sc.nextLine();
-        System.out.println("Nhập ngày sinh giảng viên: ");
-        LocalDate birthDate = LocalDate.parse(sc.nextLine());
-        System.out.println("Nhập email giảng viên: ");
-        String email = sc.nextLine();
-        System.out.println("Nhập lương giảng viên: ");
-        double salary = Double.parseDouble(sc.nextLine());
+        String name;
+        do {
+            System.out.println("Nhập tên giảng viên: ");
+            name = sc.nextLine();
+        } while (!isValidName(name));
+        LocalDate birthDate;
+        do {
+            System.out.println("Nhập ngày sinh giảng viên: ");
+            birthDate = LocalDate.parse(sc.nextLine());
+        } while (!isValidBirthDate(birthDate));
+        String email;
+        do {
+            System.out.println("Nhập email giảng viên: ");
+            email = sc.nextLine();
+        } while (!isValidEmail(email));
+        double salary;
+        do {
+            System.out.println("Nhập lương giảng viên: ");
+            salary = Double.parseDouble(sc.nextLine());
+        } while (!isValidSalary(salary));
         Teacher newTeacher = new Teacher(code, name, birthDate, email, salary);
         teacherService.addTeacher(newTeacher);
         System.out.println("Đã thêm mới thành công!");
@@ -42,17 +56,29 @@ public class TeacherController {
         String code = sc.nextLine();
         Teacher teacher = teacherService.findTeacherByCode(code);
         if (teacher != null) {
-            System.out.println("Nhập tên giảng viên: ");
-            String name = sc.nextLine();
+            String name;
+            do {
+                System.out.println("Nhập tên giảng viên: ");
+                name = sc.nextLine();
+            } while (!isValidName(name));
             teacher.setName(name);
-            System.out.println("Nhập ngày sinh giảng viên: ");
-            LocalDate birthDate = LocalDate.parse(sc.nextLine());
+            LocalDate birthDate;
+            do {
+                System.out.println("Nhập ngày sinh giảng viên: ");
+                birthDate = LocalDate.parse(sc.nextLine());
+            } while (!isValidBirthDate(birthDate));
             teacher.setBirthDate(birthDate);
-            System.out.println("Nhập email giảng viên: ");
-            String email = sc.nextLine();
+            String email;
+            do {
+                System.out.println("Nhập email giảng viên: ");
+                email = sc.nextLine();
+            } while (!isValidEmail(email));
             teacher.setEmail(email);
-            System.out.println("Nhập lương giảng viên: ");
-            double salary = Double.parseDouble(sc.nextLine());
+            double salary;
+            do {
+                System.out.println("Nhập lương giảng viên: ");
+                salary = Double.parseDouble(sc.nextLine());
+            } while (!isValidSalary(salary));
             teacher.setSalary(salary);
             teacherService.updateTeacher(teacher);
             System.out.println("Đã chỉnh sửa thành công!");
@@ -61,11 +87,35 @@ public class TeacherController {
         }
     }
 
-    public Teacher findTeacherByCode(String code){
+    public Teacher findTeacherByCode(String code) {
         return teacherService.findTeacherByCode(code);
     }
 
     public void deleteTeacher(Teacher teacher) {
         teacherService.deleteTeacher(teacher);
+    }
+
+    public boolean isValidName(String name) {
+        Pattern pattern = Pattern.compile("^[\\p{L} .'-]+$");
+        Matcher matcher = pattern.matcher(name);
+        return matcher.matches();
+    }
+
+    public boolean isValidBirthDate(LocalDate birthDate) {
+        Pattern pattern = Pattern.compile("^\\d{4}[-|/]\\d{2}[-|/]\\d{2}$");
+        Matcher matcher = pattern.matcher(birthDate.toString());
+        return matcher.matches();
+    }
+
+    public boolean isValidEmail(String email) {
+        Pattern pattern = Pattern.compile("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$");
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    public boolean isValidSalary(Double salary) {
+            Pattern pattern = Pattern.compile("^\\d+(\\.\\d+)?$");
+            Matcher matcher = pattern.matcher(salary.toString());
+            return  matcher.matches();
     }
 }
